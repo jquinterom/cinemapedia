893d50417fd9667636b5c429325bc309870449fe
Jhon Quintero <jfqm7710@gmail.com>
2025-10-20 21:05:10 -0500

    feat(movie-details): starting with movie details screen

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -2,6 +2,7 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_details_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
   static const String routeName = 'movie-screen';
@@ -32,6 +33,80 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
       );
     }
 
-    return Scaffold(appBar: AppBar(title: Text('Movie Id: ${widget.movieId}')));
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [_CustomSliversAppBar(movie: movie)],
+      ),
+    );
+  }
+}
+
+class _CustomSliversAppBar extends StatelessWidget {
+  final Movie movie;
+  const _CustomSliversAppBar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      shadowColor: Colors.red,
+      flexibleSpace: _FlexibleSpaceBar(movie: movie),
+    );
+  }
+}
+
+class _FlexibleSpaceBar extends StatelessWidget {
+  const _FlexibleSpaceBar({required this.movie});
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlexibleSpaceBar(
+      titlePadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      title: Text(
+        movie.title,
+        style: const TextStyle(color: Colors.white, fontSize: 20),
+        textAlign: TextAlign.start,
+      ),
+
+      background: Stack(
+        children: [
+          SizedBox.expand(
+            child: Image.network(movie.posterPath, fit: BoxFit.cover),
+          ),
+
+          SizedBox.expand(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [.7, 1.0],
+                  colors: [Colors.transparent, Colors.black87],
+                ),
+              ),
+            ),
+          ),
+
+          SizedBox.expand(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  stops: [0.0, .3],
+                  colors: [Colors.black87, Colors.transparent],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
