75832999ba3ba296d9c7890f6e259744b93a81de
Jhon Quintero <jfqm7710@gmail.com>
2025-10-28 05:18:13 -0500

    feat(drift): setup and tests of local database sucessfull

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/config/database/database.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cinemapedia/config/router/app_router.dart';
@@ -6,7 +7,28 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load(fileName: '.env');
+
+  // await db
+  //     .into(db.favoriteMovies)
+  //     .insert(
+  //       FavoriteMoviesCompanion.insert(
+  //         movieId: 1,
+  //         backdropPath: 'backdropPath.png',
+  //         originalTitle: 'originalTitle.title',
+  //         posterPath: 'posterPath.png',
+  //         title: 'title.title',
+  //       ),
+  //     );
+
+  // final deleteQuery = db.delete(db.favoriteMovies);
+  // await deleteQuery.go();
+
+  final allItems = await db.select(db.favoriteMovies).get();
+
+  print('items in database: $allItems');
 
   runApp(const ProviderScope(child: MainApp()));
 }
